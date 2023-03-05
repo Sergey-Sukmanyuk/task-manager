9d0c7d459622e96d20a371ddba19f9a074c16cfd
Serhii <ssukmanyuk@gmail.com>
2023-03-05 19:56:36 +0200

    Added controller with methods for get, post delete put data in MongoDB. Added routes

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,20 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 // const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
+const taskRoutes = require("./routes/taskRoutes")
 
 const app = express();
 
 //Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use("/api/tasks", taskRoutes);
 
 
 //Routes
 app.get("/", (req, res) => {
     res.send("Home page");
 });
-
-//Create tasks
-app.post("/api/tasks", async (req, res) => {
-    console.log(req.body);
-    res.send("Task created");
-});
-
 
 const PORT = process.env.PORT || 5000;
 
